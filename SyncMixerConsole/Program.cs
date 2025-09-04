3aa9fc41090f61971c72ec890b3cac086d376f17
MilanDeBruin <milandebruin03@gmail.com>
2025-09-05 01:52:34 +0200

    fixed console

---
+++ SyncMixerConsole/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace SyncMixerConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Syncing!...");
+            new SyncMixerLogic.SyncManager();
+
+            Console.ReadKey();
+        }
+    }
+}
